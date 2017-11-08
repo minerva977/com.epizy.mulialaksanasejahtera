7f6b3537d323e2a33b60da9ea3c0e224ee13e0bf
minerva977 <nier.vae@gmail.com>
2017-11-08 23:31:45 +0700

    new update 0.0.01

--- src/com/epizy/mulialaksanasejahtera/Controller/controllerDataDiri.java
+++ src/com/epizy/mulialaksanasejahtera/Controller/controllerDataDiri.java
@@ -93,5 +93,111 @@ public class controllerDataDiri {
         jTable.setModel(tabelModelDataDiri);
     }
 
+    public void save(JTextField jTextField1,JTextField jTextField2,JTextField jTextField3
+            ,JTextField jTextField4,JTextField jTextField5,JTextField jTextField6,JTextField jTextField7
+            ,JTextField jTextField8,JTextField jTextField9,JTextField jTextField10,JTextField jTextField11
+            ,JTextField jTextField12,JTextField jTextField13,JTextField jTextField14,JTextField jTextField15
+            ,JTextField jTextField16,JTextField jTextField17,JTextField jTextField18,JTextField jTextField19
+            ,JTextField jTextField20,JTextField jTextField21,JTextField jTextField22,JTextField jTextField23
+            ,JTextField jTextField24,JTextField jTextField25,JTextField jTextField26,JTextField jTextField27) {
+        dataDiri dataDiri = new dataDiri();
+        dataDiri.setNoRegistrasi(jTextField1.getText());
+        dataDiri.setTanggal(jTextField2.getText());
+        dataDiri.setKelamin(jTextField3.getText());
+        dataDiri.setPotonganGaji(jTextField4.getText());
+        dataDiri.setNoKTP(jTextField5.getText());
+        dataDiri.setTempatKTPDiBuat(jTextField6.getText());
+        dataDiri.setTanggalKTPDiBuat(jTextField7.getText());
+        dataDiri.setNama(jTextField8.getText());
+        dataDiri.setUsia(jTextField9.getText());
+        dataDiri.setAlamat(jTextField10.getText());
+        dataDiri.setDesaKelurahan(jTextField11.getText());
+        dataDiri.setKecamatan(jTextField12.getText());
+        dataDiri.setKabupatenKota(jTextField13.getText());
+        dataDiri.setPropinsi(jTextField14.getText());
+        dataDiri.setTempatLahir(jTextField15.getText());
+        dataDiri.setTanggalLahir(jTextField16.getText());
+        dataDiri.setNoTelpon(jTextField17.getText());
+        dataDiri.setNoHPNoFlexy(jTextField18.getText());
+        dataDiri.setTinggiBadan(jTextField19.getText());
+        dataDiri.setBeratbadan(jTextField20.getText());
+        dataDiri.setSetatus(jTextField21.getText());
+        dataDiri.setTanggalSetatus(jTextField22.getText());
+        dataDiri.setPendidikan(jTextField23.getText());
+        dataDiri.setAgama(jTextField24.getText());
+        dataDiri.setFoto(jTextField25.getText());
+        dataDiri.setEmail(jTextField26.getText());
+        dataDiri.setKeterampilanKusus(jTextField27.getText());
+        dataDiriDAO.Save(dataDiri);
+    }
 
+    public void update(JTextField jTextField1,JTextField jTextField2,JTextField jTextField3
+            ,JTextField jTextField4,JTextField jTextField5,JTextField jTextField6,JTextField jTextField7
+            ,JTextField jTextField8,JTextField jTextField9,JTextField jTextField10,JTextField jTextField11
+            ,JTextField jTextField12,JTextField jTextField13,JTextField jTextField14,JTextField jTextField15
+            ,JTextField jTextField16,JTextField jTextField17,JTextField jTextField18,JTextField jTextField19
+            ,JTextField jTextField20,JTextField jTextField21,JTextField jTextField22,JTextField jTextField23
+            ,JTextField jTextField24,JTextField jTextField25,JTextField jTextField26,JTextField jTextField27) {
+        dataDiri dataDiri = new dataDiri();
+        dataDiri.setNoRegistrasi(jTextField1.getText());
+        dataDiri.setTanggal(jTextField2.getText());
+        dataDiri.setKelamin(jTextField3.getText());
+        dataDiri.setPotonganGaji(jTextField4.getText());
+        dataDiri.setNoKTP(jTextField5.getText());
+        dataDiri.setTempatKTPDiBuat(jTextField6.getText());
+        dataDiri.setTanggalKTPDiBuat(jTextField7.getText());
+        dataDiri.setNama(jTextField8.getText());
+        dataDiri.setUsia(jTextField9.getText());
+        dataDiri.setAlamat(jTextField10.getText());
+        dataDiri.setDesaKelurahan(jTextField11.getText());
+        dataDiri.setKecamatan(jTextField12.getText());
+        dataDiri.setKabupatenKota(jTextField13.getText());
+        dataDiri.setPropinsi(jTextField14.getText());
+        dataDiri.setTempatLahir(jTextField15.getText());
+        dataDiri.setTanggalLahir(jTextField16.getText());
+        dataDiri.setNoTelpon(jTextField17.getText());
+        dataDiri.setNoHPNoFlexy(jTextField18.getText());
+        dataDiri.setTinggiBadan(jTextField19.getText());
+        dataDiri.setBeratbadan(jTextField20.getText());
+        dataDiri.setSetatus(jTextField21.getText());
+        dataDiri.setTanggalSetatus(jTextField22.getText());
+        dataDiri.setPendidikan(jTextField23.getText());
+        dataDiri.setAgama(jTextField24.getText());
+        dataDiri.setFoto(jTextField25.getText());
+        dataDiri.setEmail(jTextField26.getText());
+        dataDiri.setKeterampilanKusus(jTextField27.getText());
+        dataDiriDAO.Update(dataDiri);
+    }
+
+    public void update(JTextField NoRegistrasi) {
+        if (!NoRegistrasi.getText().trim().isEmpty()) {
+            dataDiriDAO.Delete(NoRegistrasi.getText());
+        }
+        else {
+            JOptionPane.showMessageDialog(
+                    null , "Maaf NoRegistrasi kosong mohon di isi"
+                    ,"Peringatan !!" ,JOptionPane.WARNING_MESSAGE
+            );
+            NoRegistrasi.requestFocus();
+        }
+    }
+
+    public void saerch(JTable jTable,JTextField target) {
+        dataDiriList = dataDiriDAO.GetDataByObject(target.getText());
+        tabelModelDataDiri tabelModelDataDiri = new tabelModelDataDiri(dataDiriList);
+        jTable.setModel(tabelModelDataDiri);
+    }
+
+    public void searchByOject(JTable jTable,JTextField target) {
+        if (!target.getText().trim().isEmpty()) {
+            saerch(jTable,target);
+        }
+        else {
+            JOptionPane.showMessageDialog(
+                    null , "Maaf Cari kosong mohon di isi"
+                    ,"Peringatan !!" ,JOptionPane.WARNING_MESSAGE
+            );
+            target.requestFocus();
+        }
+    }
 }
--- src/com/epizy/mulialaksanasejahtera/Main.java
+++ src/com/epizy/mulialaksanasejahtera/Main.java
@@ -1,5 +1,12 @@
 package com.epizy.mulialaksanasejahtera;
-
+/*
+    * Program Kerja Praktek PT.mulialaksanasejahtera
+    * Versi Dekotp
+    * Di Rancang untuk pendaftaran dan interview pendaftar
+    * perubhaman atau penambahan program mau pun interfacenya harus atasi izin pembuat
+    * @author  Wisnu Putra Dewa
+    * @version 0.0.1
+ */
 public class Main {
 
     public static void main(String[] args) {
